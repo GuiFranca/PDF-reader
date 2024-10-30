6e12876e934c6697eb945a4d50dac27ed7613824
Guilherme França <guilherme_capivari@hotmail.com>
2024-10-30 14:14:17 -0300

    feat: include selector

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from google.cloud import vision
-import io
 import streamlit as st
 from pdf2image import convert_from_bytes
-import os
-import numpy as np  # Adicionando a importação do numpy
 import cv2
+import numpy as np
+from google.cloud import vision
+import pytesseract
+import os
 
 # Configuração inicial
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = \
     '/Users/guilh/OneDrive/Desenvolvimento/CREDENTIALS/GOOGLE CLOUD VISION/powerful-link-184817-43039c887712.json'
 
 
+# Função para OCR usando Google Cloud Vision
 def google_cloud_vision_ocr(image):
     client = vision.ImageAnnotatorClient()
     success, encoded_image = cv2.imencode('.png', image)
@@ -26,26 +27,49 @@ def google_cloud_vision_ocr(image):
     return texts[0].description if texts else ""
 
 
-st.title("OCR com Google Cloud Vision")
+# Função para OCR usando Tesseract
+def tesseract_ocr(image):
+    return pytesseract.image_to_string(image, lang='por')
+
+
+st.title("OCR com Tesseract e Google Cloud Vision")
+
+# Upload do arquivo PDF
 uploaded_file = st.file_uploader("Envie o arquivo PDF", type="pdf")
 
 if uploaded_file:
+    # Converte o PDF em imagens
     pages = convert_from_bytes(uploaded_file.read(), dpi=300)
-    all_text = ""
+    all_text_tesseract = ""
+    all_text_google = ""
+
+    # Escolha do método de OCR
+    ocr_option = st.selectbox("Escolha o método de OCR", ["Tesseract", "Google Cloud Vision"])
 
     for page_num, page in enumerate(pages):
         page_data = np.array(page)
         page_data = cv2.cvtColor(page_data, cv2.COLOR_RGB2BGR)
 
-        # Extração com Google Cloud Vision OCR
-        ocr_text = google_cloud_vision_ocr(page_data)
-        all_text += f"\n\nPágina {page_num + 1}:\n{ocr_text}"
+        if ocr_option == "Tesseract":
+            # Extração de texto com Tesseract
+            ocr_text = tesseract_ocr(page_data)
+            all_text_tesseract += f"\n\nPágina {page_num + 1}:\n{ocr_text}"
+        else:
+            # Extração de texto com Google Cloud Vision
+            ocr_text = google_cloud_vision_ocr(page_data)
+            all_text_google += f"\n\nPágina {page_num + 1}:\n{ocr_text}"
 
-        # Exibir imagem pós-processada e download da imagem
+        # Exibir imagem pós-processada
         st.image(page_data, caption=f'Página {page_num + 1}', use_column_width=True)
 
-    # Exibir o texto completo extraído
-    st.text_area("Texto extraído:", all_text, height=300)
-
-    # Opção para baixar o texto extraído
-    st.download_button("Baixar o texto extraído", all_text, file_name="texto_extraido.txt")
+    # Exibir o texto extraído pelo métod escolhido
+    if ocr_option == "Tesseract":
+        st.text_area("Texto extraído pelo Tesseract:", all_text_tesseract, height=300)
+        # Opção para baixar o texto extraído pelo Tesseract
+        st.download_button("Baixar o texto extraído (Tesseract)", all_text_tesseract,
+                           file_name="texto_extraido_tesseract.txt")
+    else:
+        st.text_area("Texto extraído pelo Google Cloud Vision:", all_text_google, height=300)
+        # Opção para baixar o texto extraído pelo Google Cloud Vision
+        st.download_button("Baixar o texto extraído (Google Cloud Vision)", all_text_google,
+                           file_name="texto_extraido_google_vision.txt")
